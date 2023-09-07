fcbc54467c41bb04342df16ca6f695badc2ce25a
ISMWO1234 <138612512+ISMWO1234@users.noreply.github.com>
2023-09-07 09:30:06 -0400

    initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    todo items.  """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+    # function does not require to return anything
+
+#if __name__ == "__main__":
+#  print("hello")
